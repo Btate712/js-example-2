de15d17198cb03c5353a9db055a8d169c45dc6e4
Bob Tate <btate712@gmail.com>
2020-04-30 18:03:40 -0500

    removed solutions from master

--- index.js
+++ index.js
@@ -2,22 +2,37 @@
 // { coinType: "dime", year: 2014, collectorValue: 0.10}
 // coinType values include "penny", "nickel", "dime", and "quarter"
 
-// count accepts an array of coins and a string representing coin type
+// count() accepts an array of coins and a string representing coin type
 // and returns the number of coins of that type
-function count(coins, coinType) {
-  return coins.filter(coin => coin.coinType === coinType).length;
+function count() {
+  return;
 }
 
-// faceValue accepts an array of coins and a string representing coin type
+// faceValue() accepts an array of coins and a string representing coin type
 // and returns the total face value of all coins of that type (e.g. if the 
 // array of coins contains 3 quarters, faceValue(array, "quarter") would return
 // .75)
-function faceValue(coins, coinType) {
-  const faceValue = { penny: 0.01, nickel: 0.05, dime: 0.10, quarter: 0.25 };
-  console.log(faceValue[coinType]);
-  return parseFloat((count(coins, coinType) * faceValue[coinType]).toFixed(2));
+// If no value is passed for coin type, faceValue returns the total face value
+// of all the coins in the array.
+function faceValue() {
+  return;
+}
+
+// collectorValue() accepts an array of coins and returns the total collector value
+// of all the coins.
+function collectorValue(coins) {
+  return;
+}
+
+// mostValuable() accepts an array of coins and returns the most valuable coin in the array.
+// If multiple coins are tied for the highest value, any of these coins can be returned.
+
+function mostValuable(coins) {
+  return;
 }
 
 exports.count = count;
 exports.faceValue = faceValue;
+exports.collectorValue = collectorValue;
+exports.mostValuable = mostValuable;
 
